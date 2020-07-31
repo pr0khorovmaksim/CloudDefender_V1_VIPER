0ea941612f96d1f1caa890747f0fa0c442cf4bb1
pr0khorovmaksim <pr0khorovmaksim@yandex.ru>
2020-07-31 11:57:49 +0300

    Initial Commit

---
+++ CloudDefender_V1_VIPER/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CloudDefender_V1_VIPER
+//
+//  Created by maksim on 31.07.2020.
+//  Copyright © 2020 maksim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
